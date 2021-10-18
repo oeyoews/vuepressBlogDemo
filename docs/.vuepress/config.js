31d10e207bb7fc8630ca1d253a50471caae14db9
oeyoews <2956398608@qq.com>
2021-10-18 20:29:00 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,15 @@
 module.exports = {
-  //base: '/',
   title: "Vuepress ",
   description: " Just Do It!",
   head: [['link', { rel: 'icon', href: '/images/logo.svg' }]],
-  //sidebar: auto,
   lastUpdated: 'Last Updated',
   smoothcroll: true,
-  // theme config
   themeConfig: {
     logo: 'logo.svg',
     sidebar: 'auto',
     navbar: [
       { text: ' Home ', link: '/' },
-      { text: ' Config', link: '/guide/config' },
+      { text: 'Config', link: '/guide/config' },
       { text: "Theme", link: '/guide/theme' }
     ]},
   displayAllHeaders: true,
