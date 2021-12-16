d903fb5e7b085ddb4d2f2b1a93b048904f8120ab
oeyoews <jyao4783@gmail.com>
2021-12-16 12:57:57 +0800

    fix: update facicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   base: "/",
   title: "VM",
   description: " Just Do It!",
-  head: [['link', { rel: 'icon', href: '/images/logo.svg' }]],
+  head: [['link', { rel: 'icon', href: 'oeyoew.svg' }]],
   lastUpdated: 'Last Updated',
   smoothcroll: true,
   plugins: [
